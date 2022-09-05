88cc9032d1c62bebf406aafee424b1b42cae7723
Vitor Poltronieri <43485714+vitorpolm@users.noreply.github.com>
2022-09-04 21:36:57 -0300

    Verificação de login

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+/* MENU LATERAL FUNÇÃO */
+
 $('.btnAbre').click(function(){
     $('.menuLateral').toggleClass('exibe');
 });
@@ -5,3 +7,19 @@ $('.btnAbre').click(function(){
 $('.btnFecha').click(function(){
     $('.menuLateral').toggleClass('exibe');
 });
+
+
+/* VALIDAÇÃO PARA LOGIN */
+function Login(event) {
+    event.preventDefault();
+    var usuario = document.getElementsByName('email')[0].value;
+    usuario = usuario.toLowerCase();
+    var senha = document.getElementsByName('senha')[0].value;
+    senha = senha.toLowerCase();
+  
+    if (usuario == "orangenotes@orange.com" && senha == "orangenotes") {
+      window.location = "index.html";
+    }else{
+      alert("Dados incorretos, tente novamente");
+    }
+  }
